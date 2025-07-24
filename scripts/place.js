2ed43c8824cd80d61a3eacf6c6e526ae4702ada4
ADBranches <bambiedwins03@gmail.com>
2025-07-24 14:07:27 +0300

    modified:   scripts/place.js

--- scripts/place.js
+++ scripts/place.js
@@ -51,7 +51,7 @@ function renderWeather() {
 
 // ===== FOOTER ===== //
 document.querySelector("footer").innerHTML = `
-  ©${new Date().getFullYear()} Edwin Kambale Uganda 
+  &copy${new Date().getFullYear()} Edwin Kambale Uganda 
   Last Modified: ${document.lastModified}
 `;
 
