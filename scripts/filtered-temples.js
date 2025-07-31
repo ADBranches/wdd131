885edb222dbd8245e3e9bc05d336c1a44d9b52b2
ADBranches <bambiedwins03@gmail.com>
2025-07-31 16:47:09 +0300

    new file:   filtered-temples.html 	new file:   scripts/filtered-temples.js 	new file:   styles/filtered-temples.css 	modified:   styles/temples.css

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,23 @@
+document.getElementById('copyright-year').textContent = new Date().getFullYear();
+document.getElementById('last-modified').textContent = `Last Modified: ${document.lastModified}`;
+
+document.getElementById('hamburger').addEventListener('click', function() {
+  const nav = document.querySelector('nav ul');
+  const menuIcon = this.querySelector('.menu-icon');
+  const closeIcon = this.querySelector('.close-icon');
+  
+  nav.classList.toggle('show');
+  this.setAttribute('aria-expanded', nav.classList.contains('show'));
+
+  if (nav.classList.contains('show')) {
+    menuIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+    closeIcon.removeAttribute('hidden');
+  } else {
+    menuIcon.style.display = 'block';
+    closeIcon.style.display = 'none';
+    closeIcon.setAttribute('hidden', '');
+  }
+});
+
+
